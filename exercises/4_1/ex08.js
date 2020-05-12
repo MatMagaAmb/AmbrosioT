22cd719abc5a4e55277b85be0d9aacfea63b3daa
matambrosio <matmagamb1@gmail.com>
2020-05-12 18:45:20 -0300

    add ex 09 e 10

--- exercises/4_1/ex08.js
+++ exercises/4_1/ex08.js
@@ -1,5 +1,5 @@
-let num1 = 1, num2 = 2, num3 = 4;
-if (num1 % 2 !== 0 ||num2 % 2 !== 0 || num3 % 2 !== 0){
+let num1 = 5, num2 = 1, num3 = 3;
+if (num1 % 2 === 0 ||num2 % 2 === 0 || num3 % 2 === 0){
     console.log("true");
 }else{
     console.log("false");
---
+++ exercises/4_1/ex09.js
@@ -0,0 +1,6 @@
+let num1 = 2, num2 = 4, num3 = 6;
+if (num1 % 2 !== 0 ||num2 % 2 !== 0 || num3 % 2 !== 0){
+    console.log("true");
+}else{
+    console.log("false");
+}
