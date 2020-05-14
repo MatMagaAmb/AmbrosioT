e854e84f1f500cbe64d57e2a75376176696e205c
matambrosio <matmagamb1@gmail.com>
2020-05-14 15:12:50 -0300

    ex03 adicionado;

--- exercises/4_3/ex3.js
+++ exercises/4_3/ex3.js
@@ -3,18 +3,20 @@ let char2 = "";
 let space = " "
 let n = 5;
 let m = n;
-if (n > 1){
-  for(let i = 1; i <= n; i++){
-   if(i < m){
-     char2 += space;
-   }else{
-     char2 += char1;
-     console.log(char2);
- 
-     m--;
-     i = 1;
-     
-   }
+
+if (n > 0){
+  for(let i = 0; i < n; i++){
+    m -= 1;
+    for(let j = 0; j < n; j++){
+      if(j < m){
+      char2 += space;
+      }else{
+       char2 += char1;
+      }
+    }
+    console.log(char2);
+    char2 = "";
+    
   }
 
 }
